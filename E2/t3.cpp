08f921220956b0e86f6e80d7b9d23b8a135f660a
JQKonatsu <zwj1813@163.com>
2022-09-27 21:49:38 +0800

    update

--- E2/t3.cpp
+++ E2/t3.cpp
@@ -2,15 +2,14 @@
 #define MAX 2500
 int c[11][11];
 
-int visited[MAX], a[11]={1}, m, cnt;
+int visited[MAX], a[11], m, cnt;
 void f(int depth, int n, int r){
 	if(cnt>=m){
 		return;
 	}
-	for(int i=a[depth-1];i<=n;i++){
-		if(visited[i] == 0){
+	for(int i=a[depth-1]+1;i<=n;i++){
+//		if(visited[i] == 0){
 			a[depth] = i;
-			visited[i] = 1;
 			if(depth == r){
 				printf("%d ", r);
 				for(int j=1;j<=r;j++){
@@ -18,25 +17,26 @@ void f(int depth, int n, int r){
 				}
 				printf("\n");
 				cnt++;
+				if(cnt>=m)
+					break;
 			}else{
 				f(depth+1, n, r);
 			}
 			visited[i] = 0;
-		}
+//		}
 	}
 }
 
 int main(){
 	int t, n;
 	
-	c[1][0] = c[1][1] = 1;
+	c[0][0]=c[1][0] = c[1][1] = 1;
 	for(int i=2;i<=10;i++){
 		c[i][0] = 1;
 		for(int j=1;j<=i;j++){
 			c[i][j] = c[i-1][j-1] + c[i-1][j];
 		}
 	}
-	
 	scanf("%d", &t);
 	while(t--){
 		scanf("%d %d", &n, &m);
